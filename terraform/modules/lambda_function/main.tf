c85604a55c4cdf7ea8578eece7f60f9872510540
makinko <hakaseguro@gmail.com>
2025-10-07 01:44:07 +0900

    modified:   terraform/modules/lambda_function/main.tf

--- terraform/modules/lambda_function/main.tf
+++ terraform/modules/lambda_function/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "${var.function_name}_exec_role"
+  name = "${var.function_name}-exec-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
